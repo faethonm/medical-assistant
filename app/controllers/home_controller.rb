70e77d8ba40b96731f011ea63575ca33782a1b38
Faethon Milikouris <faethon.milikouris@gmail.com>
2016-05-24 00:24:08 -0700

    display appointments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: [:contact]
   def index
+    @appointments = current_user.appointments
   end
 
   def contact
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,5 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :patient
+  default_scope -> { order(:start_date) }
 end
